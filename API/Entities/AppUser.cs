905d2de0ee411c278c7668aada6b2c4910a89f91
Saesenthessis66 <lucjangruby@gmail.com>
2024-02-21 15:25:38 +0100

    Added authentication

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,13 @@
-﻿namespace API.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
 
 public class AppUser
 {
-    public int Id { get; set; }
+    public int Id { get; set; } 
+
+    [Required]
     public string UserName {get; set;}
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
