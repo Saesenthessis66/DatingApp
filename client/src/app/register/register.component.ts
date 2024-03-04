cf9126a3c6bfd8a1ebbca32034650d08aa0e0453
Saesenthessis66 <lucjangruby@gmail.com>
2024-03-05 00:27:49 +0100

    Added routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {}
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private toastr: ToastrService) {
 
    }
 
@@ -25,7 +26,7 @@ export class RegisterComponent implements OnInit {
       next: () =>{
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
