22d4f2229ee89938ecbd67cda049593d39dda981
Saesenthessis66 <lucjangruby@gmail.com>
2024-03-20 16:37:46 +0100

    Added client side registration

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -90,6 +90,12 @@ export class PhotoEditorComponent implements OnInit {
       {
         const photo = JSON.parse(response);
         this.member?.photos.push(photo);
+        if(photo.isMain && this.user && this.member)
+        {
+          this.user.photoUrl = photo.url;
+          this.member.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+        }
       }
     }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,25 +11,56 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
 
-  model: any = {}
-
-  constructor(private accountService: AccountService, private toastr: ToastrService) {
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) {
 
    }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear()-18);
+  }
 
+  initializeForm()
+  {
+    this.registerForm = this.fb.group({
+      username: ['',Validators.required],
+      gender: ['male'],
+      knownAs: ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required],
+      password: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['',[Validators.required, this.matchValues('password')]]
+    });
+    
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn
+  {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register()
   {
-    this.accountService.register(this.model).subscribe({
-      next: () =>{
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error)
-    })
+      const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+      const values = {...this.registerForm.value, dateOfBirth: dob};
+      this.accountService.register(values).subscribe({
+        next: () =>{
+          this.router.navigateByUrl('/members')
+        },
+        error: error=>{
+          this.validationErrors = error
+        }
+      })
   }
 
   cancel()
@@ -35,4 +68,10 @@ export class RegisterComponent implements OnInit {
     this.cancelRegister.emit(false);
   }
 
+  private getDateOnly(dob: string|undefined)
+  {
+    if(!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0,10)
+  }
 }
