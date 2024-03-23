f209e055dd6b146699b79638745ef1620936ffd3
Saesenthessis66 <lucjangruby@gmail.com>
2024-03-23 18:00:41 +0100

    Added filtering to client

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -18,6 +18,7 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination | undefined;
   userParams: UserParams | undefined;
   user: User | undefined;
+  genderList = [{value: 'male', display: 'Males'},{value:'female', display: 'Females'}]
 
   constructor(private memberService: MembersService, private accountService: AccountService) {  
     this.accountService.currentUser$.pipe(take(1)).subscribe({
@@ -47,6 +48,15 @@ export class MemberListComponent implements OnInit {
         }
       }
     })
+  }
+
+  resetFilters()
+  {
+    if(this.user)
+    {
+      this.userParams = new UserParams(this.user);
+      this.loadMembers();
+    }
   }
 
   pageChanged(event: any)
