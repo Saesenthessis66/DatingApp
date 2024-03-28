f8e785f18735c65289fe716aeb99bfe761243ce5
Saesenthessis66 <lucjangruby@gmail.com>
2024-03-28 20:29:58 +0100

    Added message entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -24,4 +24,6 @@ public class AppUser
     public List<Photo> Photos { get; set; } = new ();
     public List<UserLike> LikedByUsers { get; set; }
     public List<UserLike> LikedUsers { get; set; }
+    public List<Message> MessagesSent { get; set; }
+    public List<Message> MessageReceived { get; set; }
 }
